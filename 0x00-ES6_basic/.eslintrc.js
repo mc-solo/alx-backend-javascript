f2e487a9beb38fac3ccebdc744e08c635f39987e
mc-solo <revatoel@gmail.com>
2024-07-22 22:54:30 +0300

    ad task 2 and 3

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-module.exports = {
-  env: {
-    browser: false,
-    es6: true,
-    jest: true,
-  },
-  extends: [
-    'airbnb-base',
-    'plugin:jest/all',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['jest'],
-  rules: {
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
-    ],
-  },
-  overrides:[
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
-    }
-  ]
-=======
 
 module.exports = {
     env: {
@@ -69,5 +33,4 @@ module.exports = {
             excludedFiles: 'babel.config.js',
         }
     ]
->>>>>>> de36966c5ec71039dd091a1af27d98e5d50aa81e
 };
